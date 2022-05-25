75149051aa39dd0fe9d17ff0ad9154df9775fc85
EleazarSF <eleazarsf@gmail.com>
2022-05-25 18:01:46 -0300

    exercício 01 da aula 07

---
+++ 01-dicas-e-regras-para-variaveis.php
@@ -0,0 +1,9 @@
+<?php
+
+$nomeCompleto = 'Eleazar Freitas'; //string
+$idade = 31; //integer
+$altura = 1.70; //float
+$exercico_feito = true; // boolean
+$gosta_pizza_doce = false; // boolean
+
+?>
