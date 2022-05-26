2f26200f63a168ae3ef0658100a4bc07def58b39
EleazarSF <eleazarsf@gmail.com>
2022-05-26 14:18:47 -0300

    exercício 02 da aula 17

--- introducao-ao-php/05-while.php
+++ introducao-ao-php/05-while.php
@@ -1,4 +1,5 @@
 <select>
+
     <option selected>-- selecione o ano --</option>
     <?php
         $ano = 2022;
@@ -6,5 +7,23 @@
             echo "<option>{$ano}</option>";
             $ano--;
         }
+    ?>
+</select>
+
+<select>
+    <option selected>-- selecione o mês --</option>
+    <?php
+    for ($i = 1; $i < 13; $i++) {
+        echo "<option>{$i}</option>";
+    }
+    ?>
+</select>
+
+<select>
+    <option selected>-- selecione o dia --</option>
+    <?php
+    for ($i = 1; $i < 32; $i++) {
+        echo "<option>{$i}</option>";
+    }
     ?>
 </select>
