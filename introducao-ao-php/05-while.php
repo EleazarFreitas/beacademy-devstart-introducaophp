ab599bbea03688724470e95c23b7007b9d6630c8
EleazarSF <eleazarsf@gmail.com>
2022-05-26 13:34:19 -0300

    exercício 01 da aula 16

---
+++ introducao-ao-php/05-while.php
@@ -0,0 +1,10 @@
+<select>
+    <option selected>-- selecione o ano --</option>
+    <?php
+        $ano = 2022;
+        while ($ano >= 1900) {
+            echo "<option>{$ano}</option>";
+            $ano--;
+        }
+    ?>
+</select>
