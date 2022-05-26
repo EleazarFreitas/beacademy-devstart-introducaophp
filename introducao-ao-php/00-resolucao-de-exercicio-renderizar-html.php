fa9679965633018bbfb6635dc5ea07695e1df27f
EleazarSF <eleazarsf@gmail.com>
2022-05-26 18:44:09 -0300

    exercício calculadora atualizado

--- introducao-ao-php/00-resolucao-de-exercicio-renderizar-html.php
+++ introducao-ao-php/00-resolucao-de-exercicio-renderizar-html.php
@@ -1,32 +1,34 @@
-<form method="post">
-
-<input type="text" name="numberOne" placeholder="Número 1">
-<input type="text" name="numberTwo" placeholder="Número 2">
-
-<br>
-
-<button name="soma">Somar</button>
-<button name="subtracao">Subtrair</button>
-<button name="multiplicacao">Multiplicar</button>
-<button name="divisao">Dividir</button>
-
-</form>
-
-<?php
-
-if ($_POST) {
-    if (isset($_POST['soma'])) {
-        echo $_POST['numberOne'] + $_POST['numberTwo'];
-    }
-    if (isset($_POST['subtracao'])) {
-        echo $_POST['numberOne'] - $_POST['numberTwo'];
-    }
-    if (isset($_POST['multiplicacao'])) {
-        echo $_POST['numberOne'] * $_POST['numberTwo'];
-    }
-    if (isset($_POST['divisao'])) {
-        echo $_POST['numberOne'] / $_POST['numberTwo'];
-    }
-}
-
-?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<div class="container mt-5">
+    <h1>Calculadora</h1>
+    <hr>
+    <form method="post">
+    <input type="text" name="numberOne" placeholder="Número 1" class="form-control w-25">
+    <br>
+    <input type="text" name="numberTwo" placeholder="Número 2" class="form-control w-25">
+    <div class="mt-5">
+    <button name="soma" class="btn btn-dark">Somar</button>
+    <button name="subtracao" class="btn btn-dark">Subtrair</button>
+    <button name="multiplicacao" class="btn btn-dark">Multiplicar</button>
+    <button name="divisao" class="btn btn-dark">Dividir</button>
+    </div>
+    </form>
+    <h2>
+        <?php
+            if ($_POST) {
+                if (isset($_POST['soma'])) {
+                    echo $_POST['numberOne'] + $_POST['numberTwo'];
+                }
+                if (isset($_POST['subtracao'])) {
+                    echo $_POST['numberOne'] - $_POST['numberTwo'];
+                }
+                if (isset($_POST['multiplicacao'])) {
+                    echo $_POST['numberOne'] * $_POST['numberTwo'];
+                }
+                if (isset($_POST['divisao'])) {
+                    echo $_POST['numberOne'] / $_POST['numberTwo'];
+                }
+            }
+        ?>
+    </h2>
+</div>
