803a3b6c921533fe0cb96a417ace3e4db2b03427
EleazarSF <eleazarsf@gmail.com>
2022-05-26 14:05:00 -0300

    exercício 01 da aula 17

---
+++ introducao-ao-php/06-for.php
@@ -0,0 +1,17 @@
+<?php
+
+$quebra = '<br><br>';
+
+echo "Rei das Nações{$quebra}[...]{$quebra}";
+
+for ($i = 1; $i < 5; $i++) {
+    echo 'Magnífico, Poderoso<br>';
+}
+
+echo $quebra;
+
+for ($i = 1; $i < 5; $i++) {
+    echo 'Tu és Deus<br>';
+}
+
+echo "{$quebra}[...]";
