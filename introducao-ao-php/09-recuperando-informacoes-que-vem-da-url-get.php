b6ab0a6673aaff3650ceaefe4fe030408d486be3
EleazarSF <eleazarsf@gmail.com>
2022-05-27 11:30:53 -0300

    adicionando <h1> ao default

--- introducao-ao-php/09-recuperando-informacoes-que-vem-da-url-get.php
+++ introducao-ao-php/09-recuperando-informacoes-que-vem-da-url-get.php
@@ -6,5 +6,5 @@ echo match ($url) {
     '/' => '<h1>Página Inicial</h1>',
     '/login' => '<h1>Login</h1>',
     '/cadastro' => '<h1>Cadastro</h1>',
-    default => 'Página não encontrada'
+    default => '<h1>Página não encontrada</h1>'
 };
