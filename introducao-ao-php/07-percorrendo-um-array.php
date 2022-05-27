2b3b4c3e2e6750bd7a69e0f3a2251bffad0078b6
EleazarSF <eleazarsf@gmail.com>
2022-05-27 08:53:23 -0300

    exercício percorrendo um array atualizado

--- introducao-ao-php/07-percorrendo-um-array.php
+++ introducao-ao-php/07-percorrendo-um-array.php
@@ -1,24 +1,43 @@
-<?php
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<div class="container mt-5">
+    <?php
 
-$frutas = ['abacate','pera','ameixa','uva','manga','caju'];
+    $frutas = ['abacate','pera','ameixa','uva','manga','caju'];
 
-echo 'lista de frutas<br>';
+    echo '<h1>lista de frutas</h1><br>';
 
-foreach ($frutas as $fruta) {
-    echo "<ul><li>{$fruta}</li></ul>";
-};
+    foreach ($frutas as $fruta) {
+        echo "<ul><li>{$fruta}</li></ul>";
+    };
 
-echo 'lista de bandas e músicas conhecidas<br>';
+    ?>
 
-$bandas = [['radiohead','todas'],['keane','todas'],['coldplay','quase todas'],['muse','quase todas']];
-
-foreach ($bandas as $banda) {
-
-    echo "<ul>
-        <li>{$banda[0]}
-        <ul>
-        <li>{$banda[1]}</li>
-        </ul>
-        </li></ul>";
-    
-};
+    <h1 class="mt-5">Tabela de bandas e músicas conhecidas por mim</h1>
+    <hr>
+    <table class="table mt-5">
+        <thead>
+            <tr class="table-dark">
+                <th colspan="4">Bandas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php
+                    $bandas = [['Radiohead','Todas'],['Keane','Todas'],['Coldplay','Quase todas'],['Muse','Quase todas']];
+                    foreach ($bandas as $banda) {
+                        echo "<td>{$banda[0]}</td>";
+                    };
+                ?>
+            </tr>
+            <tr class="table-dark"><th colspan="4">Músicas conhecidas</th></tr>
+            <tr>
+                <?php
+                    $bandas = [['Radiohead','Todas'],['Keane','Todas'],['Coldplay','Quase todas'],['Muse','Quase todas']];
+                    foreach ($bandas as $banda) {
+                        echo "<td>{$banda[1]}</td>";
+                    };
+                ?>                       
+            </tr>
+        </tbody>
+    </table>
+</div>
