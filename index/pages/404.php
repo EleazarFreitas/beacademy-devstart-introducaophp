6450d76a5601886d2f1bcf4fcbba08e044664087
EleazarSF <eleazarsf@gmail.com>
2022-06-03 14:05:46 -0300

    aatualizando ditretório dos exercícios

--- index/pages/404.php
+++
@@ -1,3 +0,0 @@
-<section>
-    <img class="img-fluid" src="https://colorlib.com/cdn-cgi/image/width=1920,height=933,fit=crop,quality=80,format=auto,onerror=redirect,metadata=none/wp-content/uploads/sites/2/404-error-template-3.png">
-</section>
